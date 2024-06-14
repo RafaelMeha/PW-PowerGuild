fcf90bee1b7bd30709060a30ef57dda61807b901
Rafael Meha <97791234+RafaelMeha@users.noreply.github.com>
2024-06-14 16:53:51 +0100

    added search by id game

--- www/js/adminPage.js
+++ www/js/adminPage.js
@@ -1,6 +1,7 @@
 window.onload = async function() {
     const itemsContainer = document.getElementById('items');
     const addProductForm = document.getElementById('addProductForm');
+    const searchForm = document.getElementById('searchForm');
 
     async function fetchAndDisplayProducts() {
         try {
@@ -82,6 +83,45 @@ window.onload = async function() {
             };
         });
     }
+
+    searchForm.onsubmit = async function(event) {
+        event.preventDefault();
+        const productId = document.getElementById('searchId').value;
+        try {
+            if (productId === '') {
+                await fetchAndDisplayProducts();
+            } else {
+                const response = await fetch(`/api/products/${productId}`);
+                if (!response.ok) {
+                    if (response.status === 404) {
+                        itemsContainer.innerHTML = `No game with id: ${productId}`;
+                    } else {
+                        throw new Error('Network response was not ok');
+                    }
+                } else {
+                    const productData = await response.json();
+                    itemsContainer.innerHTML = '';
+                    const product = new Product(
+                        productData.id,
+                        productData.name,
+                        productData.description,
+                        productData.discount,
+                        productData.price,
+                        productData.quantity,
+                        productData.launch_date,
+                        productData.Type,
+                        productData.category,
+                        productData.fk_developers_id,
+                        productData.fk_suppliers_id
+                    );
+                    itemsContainer.innerHTML += product.generateHtml() + `<button class="delete-btn" data-id="${product.id}">Delete</button>`;
+                    addDeleteEventListeners();
+                }
+            }
+        } catch (error) {
+            console.error('Error searching product:', error);
+        }
+    };
 
     fetchAndDisplayProducts();
 };
