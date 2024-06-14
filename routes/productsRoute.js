d95cde4d79ee971c91fcda5c448c89aabe319d88
Rafael Meha <97791234+RafaelMeha@users.noreply.github.com>
2024-06-14 20:06:20 +0100

    things

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Platform = require("../models/productsModel");
+const Product = require("../models/productsModel");
 
 router.get('/', async function(req, res) {
     try {
@@ -15,7 +15,7 @@ router.get('/', async function(req, res) {
 router.get('/:id', async function(req, res) {
     try {
         let productId = req.params.id;
-        let result = await Platform.getById(productId);
+        let result = await Product.getById(productId);
         if (!result) {
             res.status(404).send("Product not found");
         } else {
@@ -30,7 +30,7 @@ router.get('/:id', async function(req, res) {
 router.post('/', async function(req, res) {
     try {
         let newProduct = req.body;
-        let result = await Platform.add(newProduct);
+        let result = await Product.add(newProduct);
         res.status(201).send(result);
     } catch (error) {
         console.log(error);
@@ -41,7 +41,7 @@ router.post('/', async function(req, res) {
 router.delete('/:id', async function(req, res) {
     try {
         let productId = req.params.id;
-        let result = await Platform.delete(productId);
+        let result = await Product.delete(productId);
         res.status(204).send();
     } catch (error) {
         console.log(error);
