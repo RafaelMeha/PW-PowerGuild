e94b3a67e9499d199ef18f0d9fb090d25a25c756
Rafael Meha <97791234+RafaelMeha@users.noreply.github.com>
2024-06-14 15:51:45 +0100

    removed header files

--- www/js/header.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('../header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header').insertAdjacentHTML("beforeend", data);
-        })
-        .catch(error => console.error('Error loading header:', error));
-});
