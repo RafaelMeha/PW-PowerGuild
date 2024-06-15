f9540255863a5085eb09f9fce0f9b8cf2ac12cfc
Rafael Meha <97791234+RafaelMeha@users.noreply.github.com>
2024-06-15 11:34:16 +0100

    design for products and productDetails

--- www/js/products.js
+++ www/js/products.js
@@ -14,55 +14,79 @@ class Product {
     }
 
     generateHtml() {
-        const productElement = document.createElement('li');
+        const productElement = document.createElement('div');
         productElement.classList.add('product-item');
 
         const productLink = document.createElement('a');
         productLink.href = `http://localhost:3000/html/productDetail.html?id=${this.id}`;
         productLink.style.textDecoration = 'none';
         productLink.style.color = 'inherit';
-        productLink.style.display = 'block';
+        productLink.style.display = 'flex';
+        productLink.style.alignItems = 'center';
+        productLink.style.justifyContent = 'center';
+        productLink.style.flexWrap = 'wrap';
 
-        const nameElement = document.createElement('h3');   
+        const nameElement = document.createElement('h2');       
         nameElement.textContent = this.name;
+        nameElement.style.textAlign = 'center';
+        nameElement.style.width = '100%';
         productLink.appendChild(nameElement);
+
+        const imgElement = document.createElement('img');
+        imgElement.src = `../assets/games/game.jpg`; 
+        imgElement.alt = this.name;
+        imgElement.classList.add('product-image');
+        imgElement.style.marginRight = '20px';
+        productLink.appendChild(imgElement);
+
+        const infoContainer = document.createElement('div');
 
         const descriptionElement = document.createElement('div');
         descriptionElement.textContent = `Description: ${this.description}`;
-        productLink.appendChild(descriptionElement);
+        descriptionElement.style.marginTop = '5px'
+        infoContainer.appendChild(descriptionElement);
 
         const discountElement = document.createElement('div');
         discountElement.textContent = `Discount: ${this.discount}`;
-        productLink.appendChild(discountElement);
+        discountElement.style.marginTop = '5px'
+        infoContainer.appendChild(discountElement);
 
         const priceElement = document.createElement('div');
         priceElement.textContent = `Price: ${this.price}`;
-        productLink.appendChild(priceElement);
+        priceElement.style.marginTop = '5px'
+        infoContainer.appendChild(priceElement);
 
         const quantityElement = document.createElement('div');
         quantityElement.textContent = `Quantity: ${this.quantity}`;
-        productLink.appendChild(quantityElement);
+        quantityElement.style.marginTop = '5px'
+        infoContainer.appendChild(quantityElement);
 
         const launchDateElement = document.createElement('div');
         launchDateElement.textContent = `Launch Date: ${this.launch_date}`;
-        productLink.appendChild(launchDateElement);
+        launchDateElement.style.marginTop = '5px'
+        infoContainer.appendChild(launchDateElement);
 
         const typeElement = document.createElement('div');
+        typeElement.style.marginTop = '5px'
         typeElement.textContent = `Type: ${this.type}`;
-        productLink.appendChild(typeElement);
+        infoContainer.appendChild(typeElement);
 
         const categoryElement = document.createElement('div');
         categoryElement.textContent = `Category: ${this.category}`;
-        productLink.appendChild(categoryElement);
+        categoryElement.style.marginTop = '5px'
+        infoContainer.appendChild(categoryElement);
 
         const developerIdElement = document.createElement('div');
         developerIdElement.textContent = `Developer ID: ${this.fk_developers_id}`;
-        productLink.appendChild(developerIdElement);
+        developerIdElement.style.marginTop = '5px'
+        infoContainer.appendChild(developerIdElement);
 
         const supplierIdElement = document.createElement('div');
         supplierIdElement.textContent = `Supplier ID: ${this.fk_suppliers_id}`;
-        productLink.appendChild(supplierIdElement);
+        supplierIdElement.style.marginTop = '5px'
+        infoContainer.appendChild(supplierIdElement);
 
+        productLink.appendChild(infoContainer);
         productElement.appendChild(productLink);
         return productElement;
     }
---
+++ www/js/reviewPage.js
@@ -0,0 +1,34 @@
+window.onload = async function() {
+    const params = new URLSearchParams(window.location.search);
+    const productId = params.get('id');
+    const itemsContainer = document.getElementById('reviews'); 
+
+    try {
+        const response = await fetch(`/api/reviews/${productId}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const reviewsData = await response.json();
+
+        reviewsData.forEach(reviewData => {
+            const review = new Review( 
+                reviewData.id,
+                reviewData.ratings,
+                reviewData.review_text,
+                reviewData.review_date,
+                reviewData.fk_user_id,
+                reviewData.fk_product_id
+            );
+            const reviewElement = review.generateHtml(); 
+            itemsContainer.appendChild(reviewElement);
+        });
+    } catch (error) {
+        console.error('Error fetching products:', error);
+    }
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
