39ed6f029389ade96b8156e2e61bf5b578c5c6ef
pmzpro <pedromcruz97@gmail.com>
2024-06-16 17:41:58 +0100

    still need improvement

--- www/js/review.js
+++ www/js/review.js
@@ -12,21 +12,24 @@ class Review {
         const reviewElement = document.createElement('div');
         reviewElement.classList.add('review-item');
 
-        const descriptionElement = document.createElement('div');
-        descriptionElement.textContent = `Ratings: ${this.ratings}`;
-        reviewElement.appendChild(descriptionElement);
-
-        const reviewTextElement = document.createElement('div'); 
-        reviewTextElement.textContent = `Review Text: ${this.review_text}`;
-        reviewElement.appendChild(reviewTextElement);
-
-        const reviewDateElement = document.createElement('div'); 
-        reviewDateElement.textContent = `Review Date: ${this.review_date}`;
+        const reviewDateElement = document.createElement('div');
+        reviewDateElement.textContent = `${this.review_date}`;
         reviewElement.appendChild(reviewDateElement);
+
+        const descriptionElement = document.createElement('div');
+        descriptionElement.classList.add('review-rating');
+        descriptionElement.textContent = `Rating: ${this.ratings}`;
+        reviewElement.appendChild(descriptionElement);
 
         const userIdElement = document.createElement('div'); 
         userIdElement.textContent = `User ID: ${this.fk_user_id}`;
         reviewElement.appendChild(userIdElement);
+
+        const reviewTextElement = document.createElement('div'); 
+        descriptionElement.classList.add('review-text');
+        reviewTextElement.textContent = `${this.review_text}`;
+        reviewElement.appendChild(reviewTextElement);
+
 
         return reviewElement;
     }
